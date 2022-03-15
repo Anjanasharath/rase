5e0aacd20a1c550e3c3aabbda1f2622e1d298120
anjana <anju9726@gmail.com>
2022-03-15 17:51:00 +0530

    login page, register page added

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path,include
+
+from userprofile.views import UserRegisterView
 urlpatterns=[
     path("",include('django.contrib.auth.urls')),
+    path('register/', UserRegisterView.as_view(), name='register')
     ]
--- userprofile/views.py
+++ userprofile/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 from django.contrib.auth.views import LoginView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.views.generic import CreateView
 
 class UserLoginView(LoginView):
     template_name = 'registration/login.html'
+
+class UserRegisterView(CreateView):
+    template_name = 'registration/register.html'
+    form_class = UserCreationForm
+    success_url = 'home'
